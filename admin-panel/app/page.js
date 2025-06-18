5e8268263b4d7396ede26f8e1458b901749fb94e
Sky28702 <167354173+Sky28702@users.noreply.github.com>
2025-06-19 00:50:00 +0530

    start.

---
+++ admin-panel/app/page.js
@@ -0,0 +1,11 @@
+import filename from "@/components/filename";
+
+const Home = () => {
+  return (
+    <div className=" max-w-[1024px] pl-2 pr-2 relative">
+      <filename />
+    </div>
+  );
+};
+
+export default Home;
