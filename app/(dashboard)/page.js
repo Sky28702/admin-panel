da21c48f3d08f27ceee01d641e5b229927a91207
Sky28702 <167354173+Sky28702@users.noreply.github.com>
2025-06-25 19:39:38 +0530

    basefix dashboard

--- app/(dashboard)/page.js
+++ app/(dashboard)/page.js
@@ -4,8 +4,8 @@ const Home = () => {
   return (
     <div className="bg-blue-50 min-h-screen">
       <section>
-        <div className=" flex flex-col [&>p]:hover:cursor-pointer [&>p]:hover:bg-blue-200 [&>p]:items-center [&>p]:w-50 border boder-none border-white h-full w-50 shadow shadow-slate-500">
-          <h1 className="text-blue-600 font-bold text-3xl pl-4">Title</h1>
+        <div className=" flex flex-col [&>p]:hover:cursor-pointer [&>p]:hover:bg-blue-200 [&>p]:items-center [&>p]:w-50 border boder-none border-white h-full w-80 shadow shadow-slate-500">
+          <h1 className="text-blue-600 font-bold text-3xl pl-4 pt-8">Title</h1>
           <p className="pl-4 pt-2 font-normal text-xl mt-6">
             <i className="fa-solid fa-gauge"></i> &nbsp; Dashboard
           </p>
@@ -26,7 +26,7 @@ const Home = () => {
             <i class="fa-solid fa-gear"></i> &nbsp; Setting
           </p>
 
-          <p className="relative bottom-0 mt-140  p-4">
+          <p className="relative bottom-0 mt-132  p-4">
             <i class="fa-solid fa-user"></i> &nbsp; Profile
           </p>
         </div>
