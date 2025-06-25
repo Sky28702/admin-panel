b429e7c1beb7ee564c335f092e9ffccb2bb4ec65
Sky28702 <167354173+Sky28702@users.noreply.github.com>
2025-06-25 09:00:12 +0530

    Dashboard base construction

--- app/(dashboard)/page.js
+++ app/(dashboard)/page.js
@@ -2,9 +2,35 @@
 
 const Home = () => {
   return (
-    <div className=" max-w-[1024px] pl-2 pr-2 relative">
-      {/* <filename /> */}
-      <h1>nyan uncle</h1>
+    <div className="bg-blue-50 min-h-screen">
+      <section>
+        <div className=" flex flex-col [&>p]:hover:cursor-pointer [&>p]:hover:bg-blue-200 [&>p]:items-center [&>p]:w-50 border boder-none border-white h-full w-50 shadow shadow-slate-500">
+          <h1 className="text-blue-600 font-bold text-3xl pl-4">Title</h1>
+          <p className="pl-4 pt-2 font-normal text-xl mt-6">
+            <i className="fa-solid fa-gauge"></i> &nbsp; Dashboard
+          </p>
+
+          <p className="pl-4 pt-2 font-normal text-xl mt-6">
+            <i class="fa-solid fa-server"></i> &nbsp; Data
+          </p>
+
+          <p className="pl-4 pt-2 font-normal text-xl mt-6">
+            <i class="fa-solid fa-message"></i> &nbsp; Message
+          </p>
+
+          <p className="pl-4 pt-2 font-normal text-xl mt-6">
+            <i class="fa-solid fa-bell"></i> &nbsp; Notification
+          </p>
+
+          <p className="pl-4 pt-2 font-normal text-xl mt-6">
+            <i class="fa-solid fa-gear"></i> &nbsp; Setting
+          </p>
+
+          <p className="relative bottom-0 mt-140  p-4">
+            <i class="fa-solid fa-user"></i> &nbsp; Profile
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
