b0b4d2105bd8c161f8fe4bcd9fe4006703058b24
Sky28702 <167354173+Sky28702@users.noreply.github.com>
2025-06-29 17:27:55 +0530

    DASHBOARD STYLING DONE

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,3 +1,5 @@
+"use client";
+import { usePathname } from "next/navigation";
 import { IconDashboard } from "@tabler/icons-react";
 import { IconUser } from "@tabler/icons-react";
 import { IconShoppingCartCopy } from "@tabler/icons-react";
@@ -5,49 +7,41 @@ import { IconUserCircle } from "@tabler/icons-react";
 import Link from "next/link";
 
 const layout = ({ children }) => {
+  const pathNmae = usePathname();
+
+  const getClass = (path) =>
+    `nav-list ${pathNmae === path ? "active-nav" : ""}`;
+
   return (
     <div className="flex flex-row ">
       <div className="  min-h-screen border boder-none border-white  w-80 shadow shadow-slate-500 ">
         <section>
-          <div className=" flex flex-col pl-4 [&_span]:pb-10">
+          <div className=" flex flex-col pl-4 [&_span]:pb-10 pr-4">
             <h1 class="text-black font-bold text-3xl py-6 mb-6">Admin</h1>
             <span>
               {" "}
-              <Link
-                href="/"
-                className="flex items-center font-normal text-xl bg-blue-600 flex-center whitespace-nowrap rounded-[4px] p-2 text-white"
-              >
+              <Link href="/" className={getClass("/")}>
                 <IconDashboard stroke={2} />
                 &nbsp; Dashboard
               </Link>
             </span>
 
             <span>
-              <Link
-                href="/products"
-                className="flex items-center font-normal text-xl "
-              >
+              <Link href="/products" className={getClass("/products")}>
                 <IconShoppingCartCopy stroke={2} />
-                {/* trys */}
                 &nbsp; Products
               </Link>
             </span>
 
             <span>
-              <Link
-                href="/users"
-                className="flex items-center  font-normal text-xl "
-              >
+              <Link href="/users" className={getClass("/users")}>
                 <IconUser stroke={2} />
                 &nbsp; Users
               </Link>
             </span>
 
             <span>
-              <Link
-                href="/profile"
-                className="flex items-center  font-normal text-xl "
-              >
+              <Link href="/profile" className={getClass("/profile")}>
                 <IconUserCircle stroke={2} /> &nbsp; Profile
               </Link>
             </span>
---
+++ app/(dashboard)/profile/page.js
@@ -0,0 +1,11 @@
+// import filename from "@/components/filename";
+
+const Profile = () => {
+  return (
+    <>
+      <h1>Perophile</h1>
+    </>
+  );
+};
+
+export default Profile;
