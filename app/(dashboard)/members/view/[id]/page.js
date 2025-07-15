71233dfe967626ed6d9dfd5975484e442c5aa275
Sky28702 <167354173+Sky28702@users.noreply.github.com>
2025-07-16 00:00:27 +0530

    crud done

--- app/(dashboard)/members/view/[id]/page.js
+++ app/(dashboard)/members/view/[id]/page.js
@@ -2,10 +2,9 @@
 import Link from "next/link";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "next/navigation";
 
 function ViewMembers({ params }) {
-  const [allMembers, setAllMembers] = useState();
+  const [allMembers, setAllMembers] = useState({});
 
   useEffect(() => {
     async function memberView() {
@@ -34,14 +33,14 @@ function ViewMembers({ params }) {
 
       {/* <Toaster /> */}
       <table className="[&_tr]:border [&_tr]:border-slate-200 bg-white [&_td]:p-4 shadow shadow-slate-300 rounded-[10px] w-320 text-left border-collapse ">
-        {/* <tbody>
+        <tbody>
           {Object.entries(allMembers).map(([key, value]) => (
             <tr key={key}>
               <td className="text-1xl font-medium">{key}:</td>
               <td>{value}</td>
             </tr>
           ))}
-        </tbody> */}
+        </tbody>
       </table>
     </div>
   );
