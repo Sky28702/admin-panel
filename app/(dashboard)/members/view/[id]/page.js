cfbc73819046588541cca41ab1695469c0635ef1
Sky28702 <167354173+Sky28702@users.noreply.github.com>
2025-09-17 23:56:10 +0530

    hs

--- app/(dashboard)/members/view/[id]/page.js
+++ app/(dashboard)/members/view/[id]/page.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function ViewMembers({ params }) {
-  let prisk = "ho";
+  let prisk = "hvo";
   const [allMembers, setAllMembers] = useState({});
 
   useEffect(() => {
